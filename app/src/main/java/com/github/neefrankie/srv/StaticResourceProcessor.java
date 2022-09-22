9df1a9ae9caafdb9e562902ff5132a36a2036ede
weiguo.ni <neefrankie@gmail.com>
2022-09-22 13:33:47 +0800

    change Request to HttpRequest

--- app/src/main/java/com/github/neefrankie/srv/StaticResourceProcessor.java
+++ app/src/main/java/com/github/neefrankie/srv/StaticResourceProcessor.java
@@ -1,9 +1,12 @@
 package com.github.neefrankie.srv;
 
+import com.github.neefrankie.srv.connector.http.HttpRequest;
+import com.github.neefrankie.srv.connector.http.HttpResponse;
+
 import java.io.IOException;
 
 public class StaticResourceProcessor {
-    public void process(Request req, Response res) {
+    public void process(HttpRequest req, HttpResponse res) {
         try {
             res.sendStaticResource();
         } catch (IOException e) {
